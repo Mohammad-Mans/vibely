0f7a477a765a504499eae2ab512b727ed8ea42f0
Mohammad Mansour <mohammed.i02@hotmail.com>
2024-03-12 03:20:58 +0200

    Add firebase config

---
+++ src/firebase.ts
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyArm6_kSouaM9-5eOxtUYfgTGBnrah8KLU",
+  authDomain: "vibely-dc2cb.firebaseapp.com",
+  projectId: "vibely-dc2cb",
+  storageBucket: "vibely-dc2cb.appspot.com",
+  messagingSenderId: "290646535887",
+  appId: "1:290646535887:web:7c1858ff2188df1eeb51ba",
+  measurementId: "G-TVQZG1XE6N",
+};
